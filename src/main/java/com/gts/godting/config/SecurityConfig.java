34e1a2ad88bc5c4b1f4e6b9cb331be1eab8fc48f
parkmuhyeun <pjhg410@gmail.com>
2022-03-24 21:22:42 +0900

    [#2] feat: Security 초기 설정

---
+++ src/main/java/com/gts/godting/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gts.godting.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                    .disable()
+                .formLogin()
+                    .disable()
+                .httpBasic()
+                    .disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .antMatchers("/",
+                        "/error",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                        .permitAll()
+                    .antMatchers("/", "/auth/kakao", "/auth/naver", "/auth/google")
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated();
+    }
+}
