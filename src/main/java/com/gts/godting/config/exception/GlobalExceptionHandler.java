feb2b1bf7fadcd861e669c39e2175c055e338506
pooooobi <cyh4662@icloud.com>
2022-04-18 16:49:01 +0900

    feat: hibernate Exception, ExceptionHandler add

--- src/main/java/com/gts/godting/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gts/godting/config/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.gts.godting.config.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -9,8 +12,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = { CustomExcepition.class })
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomExcepition e) {
         return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class })
+    protected ResponseEntity<ErrorResponse> handleDataException() {
+        return ErrorResponse.toResponseEntity(ErrorCode.DUPLICATE_RESOURCE);
     }
 
 }
