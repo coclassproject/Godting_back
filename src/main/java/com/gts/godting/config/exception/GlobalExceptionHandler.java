bdbebe09ac77e84ba1f69f399a744262a4902c12
pooooobi <cyh4662@icloud.com>
2022-04-20 12:13:20 +0900

    fix: GlobalExceptionHandler log add

--- src/main/java/com/gts/godting/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gts/godting/config/exception/GlobalExceptionHandler.java
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static com.gts.godting.config.exception.ErrorCode.DUPLICATE_RESOURCE;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = { CustomExcepition.class })
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomExcepition e) {
+        log.error("Custom Exception throw Exception, {}" + DUPLICATE_RESOURCE);
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class })
     protected ResponseEntity<ErrorResponse> handleDataException() {
-        return ErrorResponse.toResponseEntity(ErrorCode.DUPLICATE_RESOURCE);
+        log.error("DataException throw Exception, {}", DUPLICATE_RESOURCE);
+        return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
     }
 
 }
