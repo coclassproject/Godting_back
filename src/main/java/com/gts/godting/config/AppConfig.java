c1af4a4eb895354e06ce060917ca9ed203025767
pooooobi <cyh4662@icloud.com>
2022-04-21 19:36:55 +0900

    미사용 클래스 제거

--- src/main/java/com/gts/godting/config/AppConfig.java
+++ src/main/java/com/gts/godting/config/AppConfig.java
@@ -1,19 +1,7 @@
 package com.gts.godting.config;
 
-import com.gts.godting.config.auth.UserDetailsServiceImpl;
-import com.gts.godting.config.auth.token.JwtTokenProvider;
-import com.gts.godting.user.JpaUserRepository;
-import com.gts.godting.user.UserRepository;
-import com.gts.godting.user.UserService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.NameTokenizers;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.NameTokenizers;
-import org.modelmapper.spi.NameTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
